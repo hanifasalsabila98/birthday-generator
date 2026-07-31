f2bb3a69b12856b9585a43e9a38d52192605d580
hanifasalsabila98 <hanifa.salsabilaa98@gmail.com>
2026-07-31 20:02:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -60,6 +60,10 @@ function loadBirthdayData() {
     // AUDIO
     // ======================================
 
+	console.error("Intro:", birthdayData.music.intro);
+	console.error("Background:", birthdayData.music.background);
+	console.error("Congrats:", birthdayData.music.congratulations);
+
     introBgm.src = birthdayData.music.intro;
     bgm.src = birthdayData.music.background;
     congrats.src = birthdayData.music.congratulations;
